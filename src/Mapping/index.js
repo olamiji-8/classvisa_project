6f57dcd37b79824b581da9e22d159fefcbe63a8f
olamiji-8 <olamijisotomi@gmail.com>
2022-10-13 21:38:40 +0100

    Mapping

--- src/Mapping/index.js
+++ src/Mapping/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 import './index.css';
 
-const index = () => {
+function Index() {
+    const [people, setPeople] = useState(data)
   return (
-    <div>index</div>
+    <div className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+    </div>
   )
 }
 
-export default index
+export default Index
+
+
+
+
+
